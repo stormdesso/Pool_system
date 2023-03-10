db16fffd8e42bac12eeaa239b2b444283328957c
stormdesso <stormdesso@gmail.com>
2023-03-10 18:26:40 +0500

    test

--- Pool_system/Controllers/AuthorizationController.cs
+++ Pool_system/Controllers/AuthorizationController.cs
@@ -26,7 +26,7 @@ namespace Pool_system.Controllers
                     return View("dataCheck");//авторизован успешно
                 }
                 else
-                    return View("Index");//пользователь не найден. Хорошо!!!!
+                    return View("Index");//пользователь не найден. Хорошо!!!!ТЕст успешный
             }
             catch (Exception ex)
             {
