bcb508eed1c2d1d97006442361c4f9facc52f45d
stormdesso <stormdesso@gmail.com>
2023-03-10 22:12:45 +0500

    add registration

--- Pool_system/Models/UserContext.cs
+++ Pool_system/Models/UserContext.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Pool_system.Models
 {
@@ -54,27 +55,32 @@ namespace Pool_system.Models
             using (MySqlConnection conn = GetConnection())
             {
                 conn.Open();
-                MySqlParameter param = new MySqlParameter("@login", login);
+                MySqlParameter loginParam = new MySqlParameter("@login", login);
                 MySqlParameter passswordParam = new MySqlParameter("@password", password);
-                MySqlCommand cmd = new MySqlCommand(
+                MySqlCommand getRecordCmd = new MySqlCommand(
                     "SELECT count(*) FROM users\r\n\t" +
                     "WHERE (users.Login = @login);"
                 , conn);
 
-                cmd.Parameters.Add(param);
+                getRecordCmd.Parameters.Add(loginParam);
 
-                using (MySqlDataReader reader = cmd.ExecuteReader())
-                {                    
-                    while (reader.Read())
-                    {
-                        if (login == reader.GetString("Login"))
-                        {
-                            conn.Close();
-                            return false;
-                        }
-                    }
+                int numberOfRecords = 0;
+                using (MySqlDataReader reader = getRecordCmd.ExecuteReader())
+                {
+                    numberOfRecords = reader.GetInt16("COUNT(*)");
                 }
 
+                if (numberOfRecords == 0) 
+                {
+                    var insertCmd = new MySqlCommand("INSERT INTO users (Login, Password) VALUES ();");
+                    insertCmd.Parameters.Add(loginParam);
+                    insertCmd.Parameters.Add(passswordParam);
+
+                    if (insertCmd.ExecuteNonQuery() != 1) 
+                    { 
+                        return false;
+                    }   
+                }
             }
 
             return false;
--- Pool_system/Program.cs
+++ Pool_system/Program.cs
@@ -4,16 +4,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//test
+builder.Services.AddSwaggerGen();
+//test
 builder.Services.Add(new ServiceDescriptor(typeof(UserContext), new UserContext(builder.Configuration.GetConnectionString("DefaultConnection"))));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+if (!app.Environment.IsDevelopment()){
+
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    //test
+    app.UseSwagger();
+    app.UseSwaggerUI( 
+        
+        );
+    //test
 }
 
 app.UseHttpsRedirection();
