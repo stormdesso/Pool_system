c95d07b0e94c0dd47cc1b2d5489d8c64577c5548
Денис Пеленев <xomagtsimons@gmail.com>
2023-03-11 03:15:50 +0500

    Добавил форму регистрации, указал в контроллере Registration путь к данным пользователя, добавил прямую и обратную связь между окном регистрации и авторизации, добавил страницу для восстановления пароля и указал в ней путь к данным пользователя

--- Pool_system/Controllers/AuthorizationController.cs
+++ Pool_system/Controllers/AuthorizationController.cs
@@ -7,8 +7,20 @@ namespace Pool_system.Controllers
 {
     public class AuthorizationController : Controller
     {
-
+        [HttpGet]
         public IActionResult Index() //При запуске этого контроллера, выводит html с именем index из папки Authorization (Из-за схожести названия контроллера и папки)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RegistrationForm() // Окно регистрации
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RestorePasswordForm()
         {
             return View();
         }
@@ -25,6 +37,7 @@ namespace Pool_system.Controllers
                     return View("dataCheck");//авторизован успешно
                 }
                 else
+                    @ViewData["Message"] = "Пользователь не найден";
                     return View("Index");//пользователь не найден.
             }
             catch (Exception ex)
@@ -35,20 +48,18 @@ namespace Pool_system.Controllers
 
         [HttpPost]
         [Route("registration")] //добавляет к пути registration  
-        public IActionResult Registration() //Контроллер обработки данных из формы берет поля из метода AuthorizationModel
+        public IActionResult Registration(RegistrationModel data) //Контроллер обработки данных из формы берет поля из метода AuthorizationModel
         {
             try
             {
-                AuthorizationModel data = new AuthorizationModel();
-                data.Login = "ADMIN";
-                data.Password = "ADMIN";
                 UserContext context = (UserContext)HttpContext.RequestServices.GetService(typeof(UserContext));//получаем подключение к базе
                 if (context.TryRegistrationUser(data.Login, data.Password))
                 {
-                    return View("зарегался");//зарегистрирован успешно
+                    return View("PoolList");//зарегистрирован успешно
                 }
                 else
-                    return View("не зарегался");//не зарегистрирован
+                    @ViewData["Message"] = "Пользователь с таким ФИО уже зарегистрирован"; //Поле для вывода ошибок
+                    return View("RegistrationForm");//не зарегистрирован
 
             }
             catch (Exception ex)
@@ -57,6 +68,21 @@ namespace Pool_system.Controllers
             }
         }
 
+        [HttpPost]
+        [Route("restorePassword")] //добавляет к пути registration
+        public IActionResult RestorePassword(RestorePasswordModel data) //Контроллер обработки запроса на восстановление пароля
+        {
+            if (true)
+            {
+                return View("Index");
+            }
+            else 
+            {
+                @ViewData["Message"] = "Пользователь не найден";
+                return View("RestorePasswordForm");
+            }
+            
+        }
 
 
     }
--- Pool_system/Models/AuthorizationModel.cs
+++ Pool_system/Models/AuthorizationModel.cs
@@ -1,8 +1,13 @@
-﻿namespace Pool_system.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pool_system.Models
 {
     public class AuthorizationModel
     {
+        [Display(Name = "Логин")]
         public string Login { get; set; } //Данные в поле будут поступать из html при заполнении
+
+        [Display(Name = "Пароль")]
         public string Password { get; set; } //Данные в поле будут поступать из html при заполнении       
     }
 }
