40ab35386101e13ee5d2ef5473c24c328ac5e2b1
stormdesso <stormdesso@gmail.com>
2023-03-13 02:50:40 +0500

    добавил заглушки для создания токенов

---
+++ Pool_system/Models/Classes/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Pool_system.Models.Classes
+{
+    public class User
+    {
+        public User(string sessionId, string login, string password)
+        {
+            SessionId = sessionId ?? throw new ArgumentNullException(nameof(sessionId));
+            Login = login ?? throw new ArgumentNullException(nameof(login));
+            Password = password ?? throw new ArgumentNullException(nameof(password));
+        }
+
+        public string SessionId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
