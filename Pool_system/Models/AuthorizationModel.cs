c3f4503378dcfaecda5aca9a6f462e94d47d6803
Денис Пеленев <xomagtsimons@gmail.com>
2023-03-12 08:42:24 +0500

    Подключен css к html с авторизацией и регистрацией (Поля выставлены друг под другом, изменен размер и цвет ошибок)

--- Pool_system/Models/AuthorizationModel.cs
+++ Pool_system/Models/AuthorizationModel.cs
@@ -5,9 +5,11 @@ namespace Pool_system.Models
     public class AuthorizationModel
     {
         [Display(Name = "Логин")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public string Login { get; set; } //Данные в поле будут поступать из html при заполнении
 
         [Display(Name = "Пароль")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         public string Password { get; set; } //Данные в поле будут поступать из html при заполнении       
     }
 }
--- Pool_system/Models/RegistrationModel.cs
+++ Pool_system/Models/RegistrationModel.cs
@@ -7,18 +7,25 @@ namespace Pool_system.Models
     {
         [Display(Name = "Логин")]
         public string Login { get; set; } //Логин
+
         [Display(Name = "Пароль")]
         public string Password { get; set; } //Пароль
+
         [Display(Name = "ФИО")]
         public string Name { get; set; } //ФИО
+
         [Display(Name = "Электронная почта")]
         public string Email { get; set; } //Электронная почта
+
         [Display(Name = "Населенный пункт")]
         public string Locality { get; set; } //Населенный пункт
+
         [Display(Name = "Название улицы")]
         public string StreetName { get; set; } //Название улицы
+
         [Display(Name = "Номер дома")]
         public string HouseNumber { get; set; } //Номер дома
+
         [Display(Name = "Номер квартиры")]
         public string ApartmentNumber { get; set; } //Номер квартиры
 
