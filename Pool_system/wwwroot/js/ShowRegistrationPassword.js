aabdcdc467c4d6eb3c2cd9bd9aab76e93ae9cc1f
Денис Пеленев <xomagtsimons@gmail.com>
2023-03-14 19:46:47 +0500

    Исправлена ошибка: В окне регистрации не работали кнопки "Показать пароль"

--- Pool_system/wwwroot/js/ShowRegistrationPassword.js
+++ Pool_system/wwwroot/js/ShowRegistrationPassword.js
@@ -1,19 +1,27 @@
-let btnPass = document.querySelector('.js-btn-password')
+let btnPass1 = document.querySelector('.js-btn-password1'),
+    inputPass1 = document.querySelector('#js-first-password'),
+    btnPass2 = document.querySelector('.js-btn-password2'),
+    inputPass2 = document.querySelector('#js-second-password');
 
-btnPass.forEach(function (btn) {
-    btn.onclick = function () {
-        let target = this.getAttribute('data-target'),
-            inputPass = document.querySelector(target);
-
-        if (inputPass.getAttribute('type') == 'password') {
-            btn.classList.add('active');
-            inputPass.setAttribute('type', 'text');
-        }
-        else {
-            btn.classList.remove('active');
-            inputPass.setAttribute('type', 'password');
-        }
-
+btnPass1.onclick = function () {
+    if (inputPass1.getAttribute('type') == 'password') {
+        btnPass1.classList.add('active');
+        inputPass1.setAttribute('type', 'text');
     }
-});
+    else {
+        btnPass1.classList.remove('active');
+        inputPass1.setAttribute('type', 'password');
+    }
+}
+
+btnPass2.onclick = function () {
+    if (inputPass2.getAttribute('type') == 'password') {
+        btnPass2.classList.add('active');
+        inputPass2.setAttribute('type', 'text');
+    }
+    else {
+        btnPass2.classList.remove('active');
+        inputPass2.setAttribute('type', 'password');
+    }
+}
 
